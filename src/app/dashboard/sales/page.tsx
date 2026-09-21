ec8e94467e21b684b3e6f34796af1e5c95962811
David Bailly <david.bailly.dev@proton.me>
2026-09-21 20:04:44 +0200

    feat: ajoute le calcul et l'affichage du taux de remboursement

--- src/app/dashboard/sales/page.tsx
+++ src/app/dashboard/sales/page.tsx
@@ -1,7 +1,22 @@
+import { CardGroup, CardGroupTitle, CardGroupContent, Card, CardTitle, CardContent } from "../../../components/ui/Card";
 import { PageTitle } from "../../../components/ui/PageTitle";
+import { RefundRate } from "../../../components/dashboard/RefundRate";
 
 export default function SalesPage() {
     return (
-        <PageTitle>Ventes</PageTitle>
+        <div className="space-y-8">
+            <PageTitle>Ventes</PageTitle>
+            <CardGroup>
+                <CardGroupTitle>Statuts des commandes</CardGroupTitle>
+                <CardGroupContent>
+                    <Card>
+                        <CardTitle>Taux de remboursement</CardTitle>
+                        <CardContent>
+                            <RefundRate />
+                        </CardContent>
+                    </Card>
+                </CardGroupContent>
+            </CardGroup>
+        </div>
     );
 }
